f16cfb70e9f5f46cc341aab214fbcd6f44c915c1
Cheyanna <cheyanna.graham@gmail.com>
2019-02-10 23:20:18 -0800

    refactor month.js with hooks

--- src/Components/Month/Month.js
+++ src/Components/Month/Month.js
@@ -1,54 +1,32 @@
 import React from "react";
 import { MONTHS } from "./Helper";
 
-class Month extends React.Component {
-  //constructor & CDM only happens once. after rerender, it
-  //is not invoked when props or state changes
+export default function Month(props) {
+  return (
+    <>
+      <h2>{MONTHS[props.month]}</h2>
 
-  // getBusinesses = () => {
-  //     const business = this.props.month;
-  //     for(let bus in business) {
-  //         console.log(bus,business[bus])
-  //     }
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Business</th>
+            <th>Amount</th>
+            <th>Type</th>
+          </tr>
+        </thead>
 
-  // }
-
-  render() {
-    return (
-      <>
-        <h2>{MONTHS[this.props.month]}</h2>
-
-        <table>
-          <thead>
+        <tbody>
+          {props.transactions.map(trans => (
             <tr>
-              <th>Date</th>
-              <th>Business</th>
-              <th>Amount</th>
-              <th>Type</th>
+              <td>{trans["date"]}</td>
+              <td>{trans["business"]}</td>
+              <td>{trans["amount"]}</td>
+              <td>{trans["type"]}</td>
             </tr>
-          </thead>
-
-          <tbody>
-              {this.props.transactions.map(trans => (
-                <tr>
-                    <td>{trans['date']}</td>
-                    <td>{trans['business']}</td>
-                    <td>{trans['amount']}</td>
-                    <td>{trans['type']}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </>
-    );
-  }
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
-
-export default Month;
-
-// trans.map(tran => {
-//     <h3>Purchases</h3>
-//     <h3>Income</h3>
-//     <h3>Balance</h3>
-
-// })
