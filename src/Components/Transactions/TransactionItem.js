c35657163886d40f2f577ec7f3ba2691cf0a69f7
Cheyanna <cheyanna.graham@gmail.com>
2019-03-21 23:42:44 -0700

    style title headers and month header

--- src/Components/Transactions/TransactionItem.js
+++ src/Components/Transactions/TransactionItem.js
@@ -6,31 +6,31 @@ import moment from 'moment';
 
 const TransactionItem = props => {
 
-    return (
-        <>
-            <ul className={style.list}>
+  return (
+    <>
+      <ul className={style.list}>
 
-              <li>
-                <button className={style.button} onClick={() => props.updateTransaction(props.trans)}>
-                  <i className={`material-icons ${style.icon}`}> edit </i>
-                </button>
-              </li>
+        <li>
+          <button className={style.button} onClick={() => props.updateTransaction(props.trans)}>
+            <i className={`material-icons ${style.icon}`}> edit </i>
+          </button>
+        </li>
 
-              <li className={style.li} >{moment(props.trans.date).format('MMM D YYYY')}</li>
-              <li className={style.li} >{props.trans.business}</li>
-              <li className={style.li} ><span>$</span>{props.trans.amount}</li>
+        <li className={style.li} >{moment(props.trans.date).format('MMM D YYYY')}</li>
+        <li className={style.li} >{props.trans.business}</li>
+        <li className={style.li} ><span>$</span>{props.trans.amount}</li>
 
-              <li>
-                <button className={style.button} onClick={() => props.confirmDelete(props.trans.id)}>
-                  <i className={`material-icons ${style.icon}`}> delete </i>
-                </button>
-              </li>
+        <li>
+          <button className={style.button} onClick={() => props.confirmDelete(props.trans.id)}>
+            <i className={`material-icons ${style.icon}`}> delete </i>
+          </button>
+        </li>
 
-            </ul>
+      </ul>
 
-            <hr className = {style.hr}></hr>
-        </>
-    )
+      <hr className={style.hr}></hr>
+    </>
+  )
 }
 
 export default TransactionItem;
