241cf1d17fcd164bbaaf301956e896528b32e6dd
Cheyanna <cheyanna.graham@gmail.com>
2019-04-25 15:47:20 -0700

    add Dialog MUI to Modal component

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,6 +1,10 @@
 import React, { useContext } from "react";
 import style from "../../CSS/modal.module.css";
 import { ModalContext } from "../../App";
+import Dialog from '@material-ui/core/Dialog'
+import DialogTitle  from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
 
 //possible props
 //type,status,cotent,show,callback
@@ -9,38 +13,56 @@ import { ModalContext } from "../../App";
 
 
 const Modal = props => {
+  console.log(props.content)
   const showModal = useContext(ModalContext).setShowModal;
+
+  
+  return (
+    <Dialog open = {props.content.show} >
+      <DialogTitle>
+        {props.content.type}
+      </DialogTitle>
+      
+      <DialogContent>
+        <DialogContentText>
+          {props.content.content}
+        </DialogContentText>
+      </DialogContent>
+
+
+    </Dialog>
+  )
 
 
   //Render OK button with confirm Modal. send callback to Modal to invoke on confirmation
   //Use status in class Name to display different Styles on success & failures
-  return (
-    <div id="modal" className={`${style.modal} ${style[props.content.type]} ${style[props.content.status]}`}>
+  // return (
+  //   <div id="modal" className={`${style.modal} ${style[props.content.type]} ${style[props.content.status]}`}>
 
-      <h3 className={style.header}>{props.content.status}</h3>
-      {typeof props.content.content === 'string' ?
-        (<>
-          <p className={style.p}>
-            {props.content.content}
-          </p>
-          <CloseModalButton />
-        </>
-        ) :
-        (props.content.content)
-      }
+  //     <h3 className={style.header}>{props.content.status}</h3>
+  //     {typeof props.content.content === 'string' ?
+  //       (<>
+  //         <p className={style.p}>
+  //           {props.content.content}
+  //         </p>
+  //         <CloseModalButton />
+  //       </>
+  //       ) :
+  //       (props.content.content)
+  //     }
 
-      {/*delete confirmation*/}
-      {props.content.type === "confirm" &&
-        (<button className={style.button} onClick={() => {
-          props.content.callback();
-          showModal(false);
-        }}>
-          Confirm
-        </button>
-        )}
+  //     {/*delete confirmation*/}
+  //     {props.content.type === "confirm" &&
+  //       (<button className={style.button} onClick={() => {
+  //         props.content.callback();
+  //         showModal(false);
+  //       }}>
+  //         Confirm
+  //       </button>
+  //       )}
 
-    </div>
-  );
+  //   </div>
+  // );
 };
 
 
