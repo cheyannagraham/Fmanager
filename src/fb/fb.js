c7c8d3e7e890d4173345395f3979767ffcce9ad0
Cheyanna <cheyanna.graham@gmail.com>
2019-11-13 22:36:17 -0800

    add dotenv package

--- src/fb/fb.js
+++ src/fb/fb.js
@@ -2,17 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-    apiKey: "AIzaSyCTkSZgWGr0Obx7cwGqeAVbLUnWbIx5kLc",
-    authDomain: "fmanager-ba544.firebaseapp.com",
-    databaseURL: "https://fmanager-ba544.firebaseio.com",
-    projectId: "fmanager-ba544",
-    storageBucket: "fmanager-ba544.appspot.com",
-    messagingSenderId: "116993137159"
-  };
+
+const config = 
+{
+  apiKey: process.env['REACT_APP_KEY'],
+  databaseURL: process.env['REACT_APP_DURL'],
+  projectId: process.env['REACT_APP_PID'],
+};
 
 firebase.initializeApp(config);
-
 const db = firebase.firestore();
 
 export const auth = firebase.auth();
