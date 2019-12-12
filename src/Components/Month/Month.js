d8a017f32529677deae261575f0f6ef4846caf0e
Cheyanna <cheyanna.graham@gmail.com>
2019-12-12 09:28:14 -0800

    extract year and month state and use props instead

--- src/Components/Month/Month.js
+++ src/Components/Month/Month.js
@@ -8,15 +8,15 @@ import MonthHeader from '../MonthHeader/MonthHeader'
 
 
 const Month = props => {
-  let current = {
-    'month': moment().format("M"),
-    'year': moment().format("YYYY")
-  }
-  const [month, setMonth] = useState(current.month);
-  const [year, setYear] = useState(current.year);
+  // let current = {
+  //   'month': moment().format("M"),
+  //   'year': moment().format("YYYY")
+  // }
+  // const [month, setMonth] = useState(current.month);
+  // const [year, setYear] = useState(current.year);
   const [monthlyTransactions, setMonthlyTransactions] = useState([]);
 
-  const getMonthlyTransactions = (newMonth = month, newYear = year) => {
+  const getMonthlyTransactions = (newMonth = props.month, newYear = props.year) => {
     let mt = props.transactions.filter(transaction => {
       return (
         moment(transaction.date).format("YYYY") === String(newYear) &&
@@ -27,8 +27,9 @@ const Month = props => {
     setMonthlyTransactions(mt);
   };
 
+  //update monthly transactions if transaction list changes
   useEffect(() => {
-    getMonthlyTransactions(month);
+    getMonthlyTransactions(props.month);
   }, [props.transactions]);
 
   const handleClick = e => {
@@ -37,25 +38,25 @@ const Month = props => {
 
   // Ensure months go from Dec > Jan & Jan < Dec
   const getMonth = val => {
-    const monthVal = Number(month) + val;
+    const monthVal = Number(props.month) + val;
     let newMonth = monthVal;
-    let newYear = year;
+    let newYear = props.year;
 
     if (monthVal === 0) {
-      newYear = Number(year) - 1;
+      newYear = Number(props.year) - 1;
       newMonth = 12;
     } else if (monthVal === 13) {
-      newYear = Number(year) + 1;
+      newYear = Number(props.year) + 1;
       newMonth = 1;
     } 
-    if(year !== newYear) setYear(newYear);
-    setMonth(newMonth);
+    if(props.year !== newYear) props.setYear(newYear);
+    props.setMonth(newMonth);
     getMonthlyTransactions(newMonth, newYear);
   };
 
   return (
     <Grid container>
-      <MonthHeader handleClick = {handleClick} month = {month} year = {year} />
+      <MonthHeader handleClick = {handleClick} month = {props.month} year = {props.year} />
 
       <TransactionList
         setMonthlyTotal={props.setMonthlyTotal}
