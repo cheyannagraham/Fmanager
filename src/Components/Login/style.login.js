6a70b7ebb5819d7fab84d546d56ce1b39156108c
Cheyanna <cheyanna.graham@gmail.com>
2019-12-17 16:41:34 -0800

    extract login compoonent

---
+++ src/Components/Login/style.login.js
@@ -0,0 +1,19 @@
+const styles = {
+    'login-page-container' : {
+        margin: ' 0 auto',
+    },
+    main : {
+        'display':'flex',
+        'min-height' : '55%'
+    },
+    'button-container' : {
+        margin: 'auto',
+    },
+    header : {
+        'font-weight': '400',
+        'font-size': '3.7rem',
+        'margin-top': '15px'
+    }
+}
+
+export default styles;
