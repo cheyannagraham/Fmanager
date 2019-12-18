fd6eb0a533108eb76b46f5989f5c64391314b17b
Cheyanna <cheyanna.graham@gmail.com>
2019-12-18 10:14:15 -0800

    refactor/extract signout component

---
+++ src/Components/Signout/Signout.js
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import Button from "@material-ui/core/Button";
+import { ModalContext } from "../../App";
+import { auth } from "../../fb/fb";
+import { CloseModalButton } from "../Modal/Modal";
+
+const Home = props => {
+  const showModal = useContext(ModalContext);
+
+  const signout = () => {
+    auth.signOut().catch(err =>
+      showModal({
+        show: true,
+        type: "error",
+        title: "Error Signing Out!",
+        text: err,
+        actions: <CloseModalButton autofocus={true} variant="contained" />
+      })
+    );
+  };
+
+  return (
+    <Button onClick={signout} color="secondary">
+      Signout
+    </Button>
+  );
+};
+
+export default Home;
