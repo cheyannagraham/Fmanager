d6bee06f0e59619b2e6f7605e4b24380f3cc3fdc
Cheyanna <cheyanna.graham@gmail.com>
2019-12-18 10:04:40 -0800

    refactor signup component

---
+++ src/Components/Signup/Signup.js
@@ -0,0 +1,85 @@
+import React, { useContext } from "react";
+import { ModalContext } from "../../App";
+import Button from "@material-ui/core/Button";
+import { Email, Password, Username } from '../FormInputs/FormInputs';
+import { CloseModalButton } from '../Modal/Modal';
+import { auth } from "../../fb/fb";
+
+
+const SignupButton = props => {
+  const showModal = useContext(ModalContext).setShowModal;
+
+  const showForm = () => {
+    showModal({
+      show: true,
+      type: "signup",
+      title: "Create An Account",
+      content: (
+        <SignupForm />
+      )
+    });
+  };
+  return (
+    <Button
+      size="medium"
+      onClick={showForm}
+      color="primary"
+      variant="contained"
+    >
+      SignUp
+    </Button>
+  );
+};
+
+export default SignupButton;
+
+
+export const SignupForm = props => {
+  const showModal = useContext(ModalContext).setShowModal;
+
+  const handleSignup = async e => {
+    e.preventDefault();
+		const form = document.querySelector("#signup-form");
+    const [username, email, pwd] = [form["username"].value, form["email"].value, form["pwd"].value];
+    const displayName = username ? (username[0].toUpperCase() + username.slice(1)).trim() : "";
+
+    await auth.createUserWithEmailAndPassword(email, pwd)
+      .then(async () => {
+        //add username to profile
+        await auth.currentUser.updateProfile({ displayName: displayName });
+      })
+      .catch(err =>
+        showModal({
+          show: true,
+          type: "error",
+          title: "Signup Error!",
+          text: (
+            <>
+              <strong>{err.code} :</strong>
+              <br></br>
+              <br></br>
+              {err.message}
+            </>
+          ),
+          actions: <CloseModalButton variant="contained" autofocus={true} />
+        })
+      );
+    showModal(false);
+  };
+
+  const variant = "filled";
+
+  return (
+    <form id="signup-form" onSubmit={handleSignup}>
+      <Username autofocus={true} variant={variant} />
+      <Email variant={variant} />
+      <Password variant={variant} />
+      <div>
+        <Button variant="contained" color="primary" type="submit">
+          Signup
+        </Button>
+        <CloseModalButton />
+      </div>
+    </form>
+  );
+};
