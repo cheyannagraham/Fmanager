67173135dfb4c845514a3341099847fa8811b0e0
Cheyanna <cheyanna.graham@gmail.com>
2020-01-15 10:12:34 -0800

    rename monthheader

---
+++ src/Components/MonthlyViewHeader/styles.monthlyviewheader.js
@@ -0,0 +1,17 @@
+const styles = {
+  "month-header": {
+    margin: "15px auto 10px"
+  },
+  "month-title": {
+    "font-size": "1.5em"
+  },
+  icon: {
+    "font-size": "2em",
+    padding: "0"
+  },
+  ibutton: {
+    padding: "0"
+  }
+};
+
+export default styles;
