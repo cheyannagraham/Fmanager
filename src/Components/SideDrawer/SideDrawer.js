4fbbf50e50eba91ed633e42fa9168ac513668096
Cheyanna <cheyanna.graham@gmail.com>
2020-01-23 10:50:12 -0800

    makes drawer responsive

--- src/Components/SideDrawer/SideDrawer.js
+++ src/Components/SideDrawer/SideDrawer.js
@@ -1,23 +1,28 @@
-import React, { useContext } from "react";
+import React from "react";
 import Drawer from "@material-ui/core/Drawer";
-import Box from "@material-ui/core/Box";
-import useTheme from '@material-ui/core/styles/useTheme';
 import TopBarSpacer from "../TopBarSpacer/TopBarSpacer";
+import { Hidden } from "@material-ui/core";
 
 const SideDrawer = props => {
-    const theme = useTheme();
-    const styles = {
-        toolbar: theme.mixins.toolbar
-    }
-    
-    return (
+  return (
+    <>
+    {/* Mobile Display */}
+      <Hidden mdUp={true}>
         <Drawer variant="permanent" open={true}>
-            <TopBarSpacer/>
-            <p>yo</p>
+          <TopBarSpacer />
+          <p>small</p>
         </Drawer>
+      </Hidden>
 
-    )
-
-}
+      {/* Not Mobile Display */}
+      <Hidden smDown={true}>
+        <Drawer variant="permanent" open={true}>
+          <TopBarSpacer />
+          <p>large</p>
+        </Drawer>
+      </Hidden>
+    </>
+  );
+};
 
 export default SideDrawer;
