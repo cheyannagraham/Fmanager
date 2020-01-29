47457d37870b1dc4b0a07cd5072fc04aba766f9c
Cheyanna <cheyanna.graham@gmail.com>
2020-01-29 15:47:52 -0800

    add bottom bar

---
+++ src/Components/BottomBar/BottomBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import styles from "./styles.bottombar";
+
+const BottomBar = props => {
+  const { classes } = props;
+
+  return (
+    <AppBar position="fixed" className={classes.bottom}>
+      {/* <Toolbar variant="dense" disableGutters> */}
+        {props.children}
+      {/* </Toolbar> */}
+    </AppBar>
+  );
+};
+
+export default withStyles(styles)(BottomBar);
+
---
+++ src/Components/BottomBar/styles.bottombar.js
@@ -0,0 +1,11 @@
+const styles = {
+  avatar: {
+    background: "transparent"
+  },
+  bottom: {
+    top: "auto",
+    bottom: 0,
+  }
+};
+
+export default styles;
