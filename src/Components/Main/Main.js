3cba9ea6f543a275e246dccc8bd202b2ba160628
Cheyanna <cheyanna.graham@gmail.com>
2020-01-31 10:55:32 -0800

    add noistack package for snackbar

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,14 +2,13 @@ import React, { useState } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 import AddButton from "../AddButton/AddButton";
 import styles from "./styles.main";
 import TopBar from "../TopBar/TopBar";
 import BarSpacer from "../BarSpacer/BarSpacer";
 import WithView from "../WithView/WithView";
 import FilteredView from "../FilteredView/FilteredView";
-
-import Box from "@material-ui/core/Box";
 
 export const ViewContext = React.createContext();
 
