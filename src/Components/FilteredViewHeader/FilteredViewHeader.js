74c0c20f2350725f690ad1f27b610ab18b43c2fa
Cheyanna <cheyanna.graham@gmail.com>
2020-02-06 15:48:20 -0800

    adds labels to filter form

--- src/Components/FilteredViewHeader/FilteredViewHeader.js
+++ src/Components/FilteredViewHeader/FilteredViewHeader.js
@@ -21,43 +21,47 @@ const FilteredHeader = props => {
       <form onChange={handleChange}>
         <Grid container>
           <Grid item xs={12} sm={6}>
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              mx={2}
-            >
-              <Typography variant="h5" component="span">
-                From:
-              </Typography>
-              <FormControl
-                type="date"
-                name="start-date"
-                id="start-date"
-                label=""
-                value={moment().format("YYYY-MM-DD")}
-                required
-              />
-            </Box>
+            <label>
+              <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                mx={2}
+              >
+                <Typography variant="h5" component="span">
+                  From:
+                </Typography>
+                <FormControl
+                  type="date"
+                  name="start-date"
+                  id="start-date"
+                  label=""
+                  value={moment().format("YYYY-MM-DD")}
+                  required
+                />
+              </Box>
+            </label>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              mx={2}
-            >
-              <Typography variant="h5" component="span">
-                To:
-              </Typography>
-              <FormControl
-                type="date"
-                name="end-date"
-                id="end-date"
-                value={moment().format("YYYY-MM-DD")}
-                required
-              />
-            </Box>
+            <label>
+              <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                mx={2}
+              >
+                <Typography variant="h5" component="span">
+                  To:
+                </Typography>
+                <FormControl
+                  type="date"
+                  name="end-date"
+                  id="end-date"
+                  value={moment().format("YYYY-MM-DD")}
+                  required
+                />
+              </Box>
+            </label>
           </Grid>
         </Grid>
       </form>
