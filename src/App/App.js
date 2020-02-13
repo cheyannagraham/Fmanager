c6108c39252a07bc5e284cb06d715db2311de6d3
Cheyanna <cheyanna.graham@gmail.com>
2020-02-13 11:46:17 -0800

    removes styles from app component

--- src/App/App.js
+++ src/App/App.js
@@ -1,34 +1,29 @@
 import React, { useState, useEffect } from "react";
 import {
   ThemeProvider,
-  createMuiTheme,
-  withStyles
 } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
-import myPalette from "../CSS/mypalette";
 import { auth } from "../fb/fb";
-import styles from "./styles.app";
 import Modal from "../Components/Modal/Modal";
 import { getTransactions } from "../Components/Helpers/DBHelper";
 import Main from "../Components/Main/Main";
 import LandingPage from "../Components/LandingPage/LandingPage";
 import { shiftQueue } from "../Components/QueueSnackbar/QueueSnackbar";
 import { SnackbarProvider } from "notistack";
+import Palette from "../CSS/Palette.js";
 
 // Global data & state
 export const ModalContext = React.createContext(false);
 export const UserContext = React.createContext(null);
 export const TransContext = React.createContext([]);
 const SnackbarRef = React.createRef();
-const theme = createMuiTheme(myPalette);
 
 const App = props => {
   const [showModal, setShowModal] = useState({ show: false });
   const [user, setUser] = useState(null);
   const [transactions, setTransactions] = useState([]);
-  const { classes } = props;
 
   auth.onAuthStateChanged(user => {
     setUser(user);
@@ -36,11 +31,11 @@ const App = props => {
 
   // update transactions if user changes
   useEffect(() => {
-    user && (async () => setTransactions(await getTransactions()))();
+    // user && (async () => setTransactions(await getTransactions()))();
   }, [user]);
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={Palette}>
       <SnackbarProvider
         TransitionProps={{direction:"up"}}
         dense
@@ -60,7 +55,6 @@ const App = props => {
               <Grid
                 container
                 justify="center"
-                className={classes["lp-content"]}
               >
                 {user ? <Main /> : <LandingPage />}
                 {showModal.show && <Modal content={showModal} />}
@@ -73,4 +67,4 @@ const App = props => {
   );
 };
 
-export default withStyles(styles)(App);
+export default App;
