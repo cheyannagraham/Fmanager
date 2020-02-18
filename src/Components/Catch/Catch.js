8769e73d909234bb3a3d20c7a0635271b1bb0912
Cheyanna <cheyanna.graham@gmail.com>
2020-02-18 13:58:22 -0800

    prevents undefined from showing in catch modal

--- src/Components/Catch/Catch.js
+++ src/Components/Catch/Catch.js
@@ -1,12 +1,16 @@
 import React from "react";
-import CloseModalButton from "../Modal/Modal";
+import { CloseModalButton } from "../Modal/Modal";
 
-const Catch = props => {
+const Catch = options => {
   return {
     show: true,
-    title: `${props.title}: ${props.err.code}`,
-    type: "error",
-    text: props.err.message,
+    title:
+      options && options.title !== undefined
+        ? `${options.title}:`
+        : options.err && options.err.code !== undefined
+        ? options.err.code
+        : options.err.name && options.err.name !== undefined && options.err.name,
+    text: options && options.message !== undefined && options.err.message,
     actions: <CloseModalButton variant="contained" autoFocus={true} />
   };
 };
