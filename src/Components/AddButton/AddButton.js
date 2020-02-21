7ddd06af870f6f5decc0799d3da71037260941bc
Cheyanna <cheyanna.graham@gmail.com>
2020-02-21 12:51:28 -0800

    replaces with fab with fab component

--- src/Components/AddButton/AddButton.js
+++ src/Components/AddButton/AddButton.js
@@ -2,7 +2,8 @@ import React, { useContext } from "react";
 import AddRounded from "@material-ui/icons/AddRounded";
 import { ModalContext } from "../../App/App";
 import TransactionForm from "../TransactionForm/TransactionForm";
-import WithFab from "../WithFab/WithFab";
+import Fab from "@material-ui/core/Fab"
+import Box from "@material-ui/core/Box"
 
 const AddButton = props => {
   const modalContent = useContext(ModalContext);
@@ -17,13 +18,17 @@ const AddButton = props => {
   };
 
   return (
-    <WithFab
+    <Box m={2}>
+
+    <Fab
       title="Add New Transaction"
       aria-label="add transaction"
-      handleClick={handleClick}
+      size="small"
+      onClick={handleClick}
     >
       <AddRounded />
-    </WithFab>
+    </Fab>
+    </Box>
   );
 };
 
