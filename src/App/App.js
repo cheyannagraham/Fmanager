08285150d46e9c0bee7cb3ee6d4f0e99253dbc0f
Cheyanna <cheyanna.graham@gmail.com>
2020-03-13 15:44:58 -0700

    removes notistack dependency

--- src/App/App.js
+++ src/App/App.js
@@ -2,24 +2,19 @@ import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
 import { auth } from "../fb/fb";
 import Modal from "../Components/Modal/Modal";
 import { getTransactions } from "../Components/Helpers/DBHelper";
 import Main from "../Components/Main/Main";
 import LandingPage from "../Components/LandingPage/LandingPage";
-//import { shiftQueue } from "../Components/QueueSnackbar/QueueSnackbar";
-// import { SnackbarProvider } from "notistack";
-import Palette from "../CSS/Palette.js";
 import Catch from "../Components/Catch/Catch";
-
 import SnackbarProvider from "../Components/SnackbarProvider/SnackbarProvider";
+import Palette from "../CSS/Palette.js";
 
 // Global data & state
 export const ModalContext = React.createContext(false);
 export const UserContext = React.createContext(null);
 export const TransContext = React.createContext([]);
-//const SnackbarRef = React.createRef();
 
 const App = props => {
   const [modalContent, setModalContent] = useState({ show: false });
@@ -44,18 +39,6 @@ const App = props => {
 
   return (
     <ThemeProvider theme={Palette}>
-      {/* <SnackbarProvider
-        TransitionProps={{ direction: "up" }}
-        dense
-        autoHideDuration={2500}
-        ref={SnackbarRef}
-        action={sbar => (
-          <Button onClick={() => SnackbarRef.current.closeSnackbar(sbar)}>
-            Close
-          </Button>
-        )}
-        onExited={shiftQueue}
-      > */}
       <SnackbarProvider>
         <UserContext.Provider value={user}>
           <TransContext.Provider value={[transactions, setTransactions]}>
