3409d10045f84d31a3925ba446f58580538dc70a
Cheyanna <cheyanna.graham@gmail.com>
2020-03-20 13:59:32 -0700

    adds fixed width to prevent overflow

--- src/Components/TransactionList/TransactionList.js
+++ src/Components/TransactionList/TransactionList.js
@@ -27,7 +27,7 @@ const TransactionList = props => {
   };
 
   return (
-    <Box maxHeight="75vh" overflowx="auto" overflow="hidden">
+    <Box maxHeight="75vh" maxWidth="80vw">
       {sortGroupedTransactions().map(group => (
         <Box my={2} key={group.date}>
           <Box borderBottom="2px solid black">
